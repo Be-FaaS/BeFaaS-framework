f9cad7f3033f10bbf00c094264633ee7ed2b00b9
Christoph Witzko <github@christophwitzko.com>
2020-05-02 23:00:14 +0200

    refactor: move to module

---
+++ lambda/main.tf
@@ -0,0 +1,122 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = var.name
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "policy" {
+  name = var.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:List*"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_exec" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_lambda_function" "fn" {
+  function_name = var.name
+
+  s3_bucket        = var.s3_bucket
+  s3_key           = var.s3_key
+  source_code_hash = var.source_code_hash
+
+  handler = var.handler
+  runtime = "nodejs12.x"
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = var.name
+}
+
+resource "aws_api_gateway_method" "root" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_rest_api.api.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "root" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_method.root.resource_id
+  http_method = aws_api_gateway_method.root.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.fn.invoke_arn
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.fn.invoke_arn
+}
+
+
+resource "aws_api_gateway_deployment" "fn" {
+  depends_on = [
+    aws_api_gateway_integration.root,
+    aws_api_gateway_integration.proxy,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "dev"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fn.function_name
+  principal     = "apigateway.amazonaws.com"
+
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
---
+++ lambda/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+}
+
+variable "s3_bucket" {
+}
+
+variable "s3_key" {
+}
+
+variable "source_code_hash" {
+}
+
+variable "handler" {
+}
