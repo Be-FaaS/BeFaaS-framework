7ba968e8d4506a7b37c5dd201a022b929f97b230
Christoph Witzko <github@christophwitzko.com>
2020-05-03 15:57:43 +0200

    fix: disable versioning

--- buckets/main.tf
+++ buckets/main.tf
@@ -3,16 +3,12 @@ locals {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = local.name
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
+  bucket        = local.name
+  acl           = "private"
+  force_destroy = true
 }
 
 resource "google_storage_bucket" "bucket" {
-  name = local.name
-  versioning {
-    enabled = true
-  }
+  name          = local.name
+  force_destroy = true
 }
