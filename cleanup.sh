a33aec7edf14ca02fb82c1d46b6c1769e99178ce
Christoph Witzko <github@christophwitzko.com>
2020-05-03 15:51:41 +0200

    chore(cleanup): remove buckets

--- cleanup.sh
+++ cleanup.sh
@@ -5,5 +5,8 @@ set -e
 bucket_name=$(cat NAME)
 
 terraform destroy
-gsutil rm -r gs://${bucket_name}/fn
-aws s3 rm --recursive s3://${bucket_name}/fn
+gsutil rm -r gs://${bucket_name}/
+aws s3 rm --recursive s3://${bucket_name}/
+
+cd buckets
+terraform destroy
