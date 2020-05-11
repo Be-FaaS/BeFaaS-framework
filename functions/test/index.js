76bdd585f91211d350292e337c6222c9c5612b17
Christoph Witzko <github@christophwitzko.com>
2020-05-11 14:14:22 +0200

    feat: function calling

--- functions/test/index.js
+++ functions/test/index.js
@@ -5,7 +5,12 @@ module.exports = lib.serverless(router => {
     ctx.body = { message: 'Hello World from test!' }
   })
 
-  router.get('/call', async (ctx, next) => {
-    ctx.body = lib.call('test')
+  router.get('/call/:provider', async (ctx, next) => {
+    ctx.body = await lib.call(ctx.params.provider, 'test2')
+  })
+
+  router.post('/call', async (ctx, next) => {
+    console.log(ctx.request.body)
+    ctx.body = { ok: true, from: 'test' }
   })
 })
--- functions/test2/index.js
+++ functions/test2/index.js
@@ -4,4 +4,13 @@ module.exports = lib.serverless(router => {
   router.get('/', (ctx, next) => {
     ctx.body = { message: 'Hello World from test2!' }
   })
+
+  router.get('/call/:provider', async (ctx, next) => {
+    ctx.body = await lib.call(ctx.params.provider, 'test')
+  })
+
+  router.post('/call', async (ctx, next) => {
+    console.log(ctx.request.body)
+    ctx.body = { ok: true, from: 'test2' }
+  })
 })
