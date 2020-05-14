def8b2fdb044421f1f5e803d775a68dd9958946e
Christoph Witzko <github@christophwitzko.com>
2020-05-14 15:52:47 +0200

    chore: add build script

---
+++ scripts/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+PKG_JSON='{"name": "fn", "main": "index.js"}'
+
+if [ -z "$1" ]; then
+    echo "usage: $0 <experiment name>"
+    exit 1
+fi
+
+exp_dir="experiments/$1/functions"
+
+if [[ ! -d $exp_dir ]]; then
+    echo "invalid experiment name"
+    exit 1
+fi
+
+echo "building experiment $1.."
+
+rm -rf $exp_dir/_build
+mkdir $exp_dir/_build
+
+for d in $exp_dir/*; do
+  fname=`basename $d`
+  if [[ "$fname" == '_build' ]]; then
+    continue
+  fi
+  echo "building ${fname}..."
+  npx ncc build $d/index.js -o $d/build
+  echo $PKG_JSON > $d/build/package.json
+  cd $d/build && zip -r ../../_build/${fname}.zip * && cd -
+  rm -rf $d/build
+  fname_list="${fname_list},${fname}"
+done
+
+echo "build done."
+
+ls -alF $exp_dir/_build
+echo $fname_list
+
