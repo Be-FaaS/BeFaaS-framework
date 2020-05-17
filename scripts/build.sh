979387e634eac1e3c1b847eff6c253d1e1a59431
Christoph Witzko <github@christophwitzko.com>
2020-05-17 21:44:27 +0200

    chore: use terminal colors for scripts 🏳️‍🌈

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,20 +5,21 @@ set -euo pipefail
 PKG_JSON='{"name": "fn", "main": "index.js"}'
 
 if [ -z "${1:-}" ]; then
-    echo "Usage: $0 <experiment name>"
-    echo "Choose one of:"
-    echo "> $(ls experiments/ | tr '\n' ' ')"
+    chalk -t "{yellow Usage: $0 }{yellow.bold <experiment name>}"
+    echo "Choose one of:" | chalk yellow
+    chalk -t "{yellow >} {yellow.bold $(ls experiments/ | tr '\n' ' ')}"
+    echo ""
     exit 1
 fi
 
 exp_dir="experiments/$1/functions"
 
 if [[ ! -d $exp_dir ]]; then
-    echo "Invalid experiment name"
+    echo -e "Invalid experiment name\n" | chalk red
     exit 1
 fi
 
-echo "Building experiment $1"
+chalk -t "{cyan Building experiment} {cyan.bold $1}"
 
 rm -rf $exp_dir/_build
 mkdir $exp_dir/_build
@@ -28,11 +29,11 @@ for d in $exp_dir/*; do
   if [[ "$fname" == '_build' ]]; then
     continue
   fi
-  echo "Going to build function: ${fname}"
+  echo "Going to build function: ${fname}" | chalk cyan
   npx ncc build $d/index.js -o $d/build
   echo $PKG_JSON > $d/build/package.json
   cd $d/build && zip -r ../../_build/${fname}.zip * && cd -
   rm -rf $d/build
 done
 
-echo "Build done"
+echo "Build done" | chalk cyan bold
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,31 +3,32 @@
 set -euo pipefail
 
 if [ -z "${1:-}" ]; then
-    echo "Usage: $0 <experiment name>"
-    echo "Choose one of:"
-    echo "> $(ls experiments/ | tr '\n' ' ')"
+    chalk -t "{yellow Usage: $0 }{yellow.bold <experiment name>}"
+    echo "Choose one of:" | chalk yellow
+    chalk -t "{yellow >} {yellow.bold $(ls experiments/ | tr '\n' ' ')}"
+    echo ""
     exit 1
 fi
 
 # Check for regiosn
 [ -z "${AWS_REGION:-}" ] && {
-    echo "Environment variable AWS_REGION should be set"
+    echo -e "Environment variable AWS_REGION should be set\n" | chalk red
     exit 1
 }
 
 [ -z "${GOOGLE_REGION:-}" ] && {
-    echo "Environment variable GOOGLE_REGION should be set"
+    echo -e "Environment variable GOOGLE_REGION should be set\n" | chalk red
     exit 1
 }
 
 # Check for credentials
 [ -z "${GOOGLE_APPLICATION_CREDENTIALS:-}" ] && {
-    echo "Environment variable GOOGLE_APPLICATION_CREDENTIALS should be set"
+    echo -e "Environment variable GOOGLE_APPLICATION_CREDENTIALS should be set\n" | chalk red
     exit 1
 }
 
 [ -z "${GOOGLE_PROJECT:-}" ] && {
-    echo "Environment variable GOOGLE_PROJECT should be set"
+    echo -e "Environment variable GOOGLE_PROJECT should be set\n" | chalk red
     exit 1
 }
 
@@ -35,11 +36,11 @@ fi
 exp_dir="experiments/$1/functions"
 
 if [[ ! -d $exp_dir ]]; then
-    echo "Invalid experiment name"
+    echo -e "Invalid experiment name\n" | chalk red
     exit 1
 fi
 
-echo "Running deploy for $1"
+chalk -t "{cyan Running deploy for} {cyan.bold $1}"
 
 cd infrastructure
 terraform apply -var "experiment=${1}"
