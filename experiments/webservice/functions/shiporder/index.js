4a4f8b3483eb50eb7b541b85c33c71dab5f88dec
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-05-19 10:53:37 +0200

    Split into two functions

---
+++ experiments/webservice/functions/shiporder/index.js
@@ -0,0 +1,83 @@
+const lib = require('@faastermetrics/lib')
+
+// Returns a new TrackingID for the shipment
+function generateTrackingID () {
+  return Math.floor(Math.random() * 999999999999)
+}
+
+// Iterates over given cart and calculates shipping cost in EUR
+function calculateShippingCost (cart) {
+  var count = 0
+  for (var i = 0; i < cart.length; i++) {
+    count += cart[i].quantity
+  }
+  // Yes, overall item quantity increases quadratically in relation to shipping cost.
+  if (count < 0) {
+    return 0
+  }
+  return Math.ceil(Math.sqrt(count))
+}
+
+/**
+ *
+ * Can either
+ *
+ * ship out a package ( /shipment ) and provide a tracking ID or
+ * calculate shipping cost of a given cart ( /shipmentquote ).
+ *
+ *
+ * Ex Payload Body: {
+ *  'address':{
+ *    'streetAddress': 'Schillerstrasse 9',
+ *    'city': 'Munich',
+ *    'state': 'Bavaria',
+ *    'country': 'Germany'
+ *  },
+ *  'items':[
+ *    {'id':1,'quantity':6},
+ *    {'id':4,'quantity':-1}
+ *  ]
+ * }
+ *
+ * Response for shipment: {
+ *   'id': <some tracking number>
+ * }
+ *
+ * Response for shipment quote: {
+ *   'costUsd': {
+ *     'currencyCode': 'USD',
+ *     'units': <shipment cost>,
+ *     'nanos': 0
+ *   }
+ * }
+ */
+
+module.exports = lib.serverless.router(router => {
+  // calculates shipping cost
+  router.post('/shipmentquote', (ctx, next) => {
+    const cart = ctx.request.body.items
+    let shippingCost = 0
+    if (cart !== undefined) {
+      shippingCost = calculateShippingCost(cart)
+    }
+
+    ctx.body = {
+      costUsd: {
+        currencyCode: 'USD',
+        units: shippingCost,
+        nanos: 0
+      }
+    }
+  })
+
+  // ships items and provides tracking number
+  router.post('/shipping', (ctx, next) => {
+    // const { address, cart } = ctx.request.body
+    ctx.body = { id: generateTrackingID() }
+  })
+
+  router.attachEventHandler(request => {
+    lib.log({ request })
+    return { ok: true, from: 'shipment' }
+  })
+})
