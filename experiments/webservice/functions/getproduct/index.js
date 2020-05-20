0c75b8a771d75fd8ab709c0459c2b0d7556c25d8
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-05-20 21:16:41 +0200

    Now triggers error if product not found

--- experiments/webservice/functions/getproduct/index.js
+++ experiments/webservice/functions/getproduct/index.js
@@ -26,5 +26,5 @@ const { products } = require('../../productcatalog/products')
 
 module.exports = lib.serverless.rpcHandler(event => {
   const id = event.id
-  return products[id]
+  return products[id] || { error: 'Product not found.' }
 })
