40bdf0544da47c38650010b0b71b221f9cba3efc
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-05-20 21:10:41 +0200

    Now GetProduct can just use the ID

--- experiments/webservice/functions/getproduct/index.js
+++ experiments/webservice/functions/getproduct/index.js
@@ -26,11 +26,5 @@ const { products } = require('../../productcatalog/products')
 
 module.exports = lib.serverless.rpcHandler(event => {
   const id = event.id
-  let product
-  for (const key in products) {
-    if (products[key].id === id) {
-      product = products[key]
-    }
-  }
-  return product
+  return products[id]
 })
