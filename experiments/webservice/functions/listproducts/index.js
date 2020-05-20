92482f2890ab480de3fec2a3bb54e0a65ba4bab6
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-05-20 19:36:56 +0200

    Fixed example notation

--- experiments/webservice/functions/listproducts/index.js
+++ experiments/webservice/functions/listproducts/index.js
@@ -8,18 +8,20 @@ const { products } = require('../../productcatalog/products')
  * Does not need a payload.
  *
  * Example Response: {
- *   "bathing_suit": {
- *     "id": "QWERTY",
- *     "name": "Bathing Suit",
- *     "description": "You will never want to take this off!",
- *     "picture": "bathing_suit.jpg",
- *     "priceUsd": {
- *       "currencyCode": "USD",
- *       "units": 64,
- *       "nanos": 990000000
- *     },
- *     "categories": ["clothing", "bath"]
- *   }
+ *   "products": [{
+ *     "bathing_suit": {
+ *       "id": "QWERTY",
+ *       "name": "Bathing Suit",
+ *       "description": "You will never want to take this off!",
+ *       "picture": "bathing_suit.jpg",
+ *       "priceUsd": {
+ *         "currencyCode": "USD",
+ *         "units": 64,
+ *         "nanos": 990000000
+ *       },
+ *       "categories": ["clothing", "bath"]
+ *     }
+ *   }]
  * }
  *
  */
