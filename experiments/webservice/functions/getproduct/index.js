46884ccca3beac6ae898ec11433a3458abdc8663
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-05-20 18:55:23 +0200

    Added GetProduct

---
+++ experiments/webservice/functions/getproduct/index.js
@@ -0,0 +1,36 @@
+const lib = require('@faastermetrics/lib')
+const { products } = require('../../productcatalog/products')
+
+/**
+ *
+ * Searches the product catalog for a given product.
+ *
+ * Example Request: {
+ *  "id": "QWERTY"
+ * }
+ *
+ * Example Response: {
+ *   "id": "QWERTY",
+ *   "name": "Bathing Suit",
+ *   "description": "You will never want to take this off!",
+ *   "picture": "bathing_suit.jpg",
+ *   "priceUsd": {
+ *     "currencyCode": "USD",
+ *     "units": 64,
+ *     "nanos": 990000000
+ *   },
+ *   "categories": ["clothing", "bath"]
+ * }
+ *
+ */
+
+module.exports = lib.serverless.rpcHandler(event => {
+  const id = event.id
+  var product
+  for (var key in products) {
+    if (products[key].id === id) {
+      product = products[key]
+    }
+  }
+  return product
+})
