c760550a99d2bc7c693401bf8782e946cdc65575
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-05-20 18:09:32 +0200

    Added List Products function

---
+++ experiments/webservice/functions/listproducts/index.js
@@ -0,0 +1,34 @@
+const lib = require('@faastermetrics/lib')
+const { products } = require('../../productcatalog/products')
+
+/**
+ *
+ * Responds with a list of all known products.
+ *
+ * Does not need a payload.
+ *
+ * Example Response: {
+ *   "bathing_suit": {
+ *     "id": "QWERTY",
+ *     "name": "Bathing Suit",
+ *     "description": "You will never want to take this off!",
+ *     "picture": "bathing_suit.jpg",
+ *     "priceUsd": {
+ *       "currencyCode": "USD",
+ *       "units": 64,
+ *       "nanos": 990000000
+ *     },
+ *     "categories": ["clothing", "bath"]
+ *   }
+ * }
+ *
+ */
+
+module.exports = lib.serverless.rpcHandler(event => {
+  // const { address, cart } = event
+  var productArray = []
+  for (var key in products) {
+    productArray.push(products[key])
+  }
+  return { productArray }
+})
