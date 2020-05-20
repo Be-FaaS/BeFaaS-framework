7680ba7701014dbb8c90d984c3bcbb4051a73e0c
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-05-20 19:33:41 +0200

    Updated Example notation

--- experiments/webservice/functions/searchproducts/index.js
+++ experiments/webservice/functions/searchproducts/index.js
@@ -6,20 +6,22 @@ const { products } = require('../../productcatalog/products')
  * Searches the product catalog for a given product.
  *
  * Example Request: {
- *  "id": "QWERTY"
+ *  "query": "BATHING"
  * }
  *
  * Example Response: {
- *   "id": "QWERTY",
- *   "name": "Bathing Suit",
- *   "description": "You will never want to take this off!",
- *   "picture": "bathing_suit.jpg",
- *   "priceUsd": {
- *     "currencyCode": "USD",
- *     "units": 64,
- *     "nanos": 990000000
- *   },
- *   "categories": ["clothing", "bath"]
+ *   "results": [{
+ *     "id": "QWERTY",
+ *     "name": "Bathing Suit",
+ *     "description": "You will never want to take this off!",
+ *     "picture": "bathing_suit.jpg",
+ *     "priceUsd": {
+ *       "currencyCode": "USD",
+ *       "units": 64,
+ *       "nanos": 990000000
+ *     },
+ *     "categories": ["clothing", "bath"]
+ *   }]
  * }
  *
  */
