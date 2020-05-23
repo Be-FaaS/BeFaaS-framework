7489490c8d4b7dc26608a0f967f2c59fe9aeb4fa
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-05-23 23:37:20 +0200

    Extended kv storage function

--- experiments/webservice/functions/cartkvstorage/index.js
+++ experiments/webservice/functions/cartkvstorage/index.js
@@ -7,7 +7,8 @@ const lib = require('@faastermetrics/lib')
  * Example Request (add): {
  *   "operation": "add",
  *   "userID": "UID6",
- *   "itemID": "QWERTY"
+ *   "itemID": "QWERTY",
+ *   "quantity": 7
  * }
  *
  * Example Request (get): {
@@ -23,14 +24,20 @@ const lib = require('@faastermetrics/lib')
  * Example Response (add): { }
  *
  * Example Response (get): {
- *   "cart": ["QWERTY"]
+ *   "items": [{
+ *     "productID": "QWERTY"],
+ *     "quantity": 7
+ *   }]
  * }
  *
  * Example Response (empty): { }
  *
  */
 
-const carts = new Map()
+// Maps userID --> itemID
+const items = new Map()
+// Maps userID x itemID --> quantity
+const quantities = new Map()
 
 module.exports = lib.serverless.rpcHandler(event => {
   const operation = event.operation.toLowerCase()
@@ -42,21 +49,40 @@ module.exports = lib.serverless.rpcHandler(event => {
     return { error: 'A user ID has to be specified.' }
   }
 
+  const tmp = []
   switch (operation) {
     case 'add':
-      if (event.itemID === undefined) {
+      if (event.itemID === undefined || event.quantity === undefined) {
         return { error: 'There is no item to be added.' }
       }
-      if (!carts.has(userID)) {
-        carts.set(userID, [event.itemID])
+      if (!items.has(userID)) {
+        items.set(userID, [event.itemID])
+        quantities.set([userID, event.itemID], event.quantity)
+      } else if (!items.get(userID).includes(event.itemID)) {
+        items.set(userID, items.get(userID).push(event.itemID))
+        quantities.set([userID, event.itemID], event.quantity)
       } else {
-        carts.set(userID, carts.get(userID).push(event.itemID))
+        quantities.set(
+          [userID, event.itemID],
+          quantities.get([userID, event.itemID]) + event.quantity
+        )
       }
       return {}
     case 'get':
-      return { cart: carts.get(userID) }
+      for (const item in items.get(userID)) {
+        tmp.push({
+          productID: item,
+          quantity: quantities.get([userID, item])
+        })
+      }
+      return {
+        items: tmp
+      }
     case 'empty':
-      carts.delete(userID)
+      for (const item in items.get(userID)) {
+        quantities.delete([userID, item])
+      }
+      items.delete(userID)
       return {}
   }
 })
