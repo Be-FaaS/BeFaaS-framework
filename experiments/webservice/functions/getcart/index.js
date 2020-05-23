d9bf963f3d6d2fd3bab9420ec099a52cc33093d6
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-05-23 23:37:58 +0200

    Added get cart function

---
+++ experiments/webservice/functions/getcart/index.js
@@ -0,0 +1,31 @@
+const lib = require('@faastermetrics/lib')
+
+/**
+ *
+ * Returns a users cart.
+ *
+ * Example Payload: {
+ *   "userID": "USER12"
+ * }
+ *
+ * Example Response: {
+ *   "userID": "USER12",
+ *   "items": [QWERTY]
+ * }
+ *
+ */
+
+module.exports = lib.serverless.rpcHandler(async event => {
+  if (event.userID === undefined) {
+    return { error: 'Wrong input format.' }
+  }
+  const cartItems = await lib.call('cartkvstorage', {
+    operation: 'get',
+    userID: event.userID
+  })
+
+  return {
+    userID: event.userID,
+    items: cartItems
+  }
+})
