f5f9fb5dc6f3d488330a563a472a85597da8e95a
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-05-25 23:54:44 +0200

    Finished cart service (kv-based)

--- experiments/webservice/functions/emptycart/index.js
+++ experiments/webservice/functions/emptycart/index.js
@@ -16,8 +16,15 @@ module.exports = lib.serverless.rpcHandler(async event => {
   if (event.userID === undefined) {
     return { error: 'Wrong input format.' }
   }
-  await lib.call('cartkvstorage', {
-    operation: 'empty',
-    userID: event.userID
-  })
+  await lib.call(
+    'cartkvstorage',
+    'ws:emptycart:' +
+      Math.random()
+        .toString(36)
+        .substr(2),
+    {
+      operation: 'empty',
+      userID: event.userID
+    }
+  )
 })
