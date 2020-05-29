847651879387a96d06616a0be8fd262d78a56e02
Max Zhao <alcasa.mz@gmail.com>
2020-05-29 17:36:02 +0200

    feat(scripts): Destroy projects

---
+++ scripts/destroy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ -z "${1:-}" ]; then
+    chalk -t "{yellow Usage: $0 }{yellow.bold <experiment name>}"
+    echo "Choose one of:" | chalk yellow
+    chalk -t "{yellow >} {yellow.bold $(ls experiments/ | tr '\n' ' ')}"
+    echo ""
+    exit 1
+fi
+
+cd infrastructure
+terraform destroy -var "experiment=${1}" -auto-approve
