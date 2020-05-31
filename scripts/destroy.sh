01003c5efd88f3bbf803dfcee7a6e19fe39df0b8
Christoph Witzko <github@christophwitzko.com>
2020-05-31 21:51:14 +0200

    feat: dynamic destroy script

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -10,5 +10,24 @@ if [ -z "${1:-}" ]; then
     exit 1
 fi
 
-cd infrastructure
+export TF_VAR_fn_env='{}'
+
+providers=$(jq -r '[.program.functions[].provider] | unique | .[]' experiments/${1}/experiment.json)
+
+for provider in $providers; do
+  echo "Destroying $provider" | chalk green
+  cd infrastructure/${provider}
+  terraform destroy -auto-approve
+  cd -
+done
+
+for provider in $providers; do
+  echo "Destroying endpoints for $provider" | chalk green
+  cd infrastructure/${provider}/endpoint
+  terraform destroy -auto-approve
+  cd -
+done
+
+cd infrastructure/experiment/
 terraform destroy -var "experiment=${1}" -auto-approve
+cd -
