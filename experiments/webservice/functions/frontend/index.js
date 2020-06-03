3ca811774890cf6b865d8762ec216ba9deab9202
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-06-03 21:10:58 +0200

    Prepared for cart inclusion

--- experiments/webservice/functions/frontend/index.js
+++ experiments/webservice/functions/frontend/index.js
@@ -11,9 +11,6 @@ const productHTML = _.template(
   fs.readFileSync(path.join(__dirname, 'html_templates', 'product.html'))
 )
 
-// TODO make this real and user-dependent
-let cartSize = 0
-
 function getSessionID (ctx) {
   if (!ctx.cookies.get('sessionId')) {
     ctx.cookies.set('sessionId', lib.helper.generateRandomID())
@@ -29,6 +26,22 @@ function getUserName (ctx) {
   return ctx.cookies.get('userName') || 'default'
 }
 
+function getCartSize (ctx) {
+  return ctx.cookies.get('cartSize') || 0
+}
+
+function increaseCartSize (ctx, inc) {
+  ctx.cookies.set('cartSize', getCartSize(ctx) + inc, { overwrite: true })
+}
+
+async function convertProductPrize (ctx, product) {
+  if (getUserCurrency(ctx) === 'USD') {
+    product.price = product.priceUsd
+  } else {
+    product.price = await ctx.lib.call('currency', {from: product.priceUsd, toCode: getUserCurrency(ctx)})
+  }
+}
+
 module.exports = lib.serverless.router(async router => {
   router.get('/', async (ctx, next) => {
     const requestId = lib.helper.generateRandomID()
@@ -36,12 +49,9 @@ module.exports = lib.serverless.router(async router => {
     const productList = (await ctx.lib.call('listproducts', {})).products
     const cats = (await ctx.lib.call('getads', {})).ads
 
+    // This one could be parallelised easily
     for (product of productList) {
-      if (getUserCurrency(ctx) === 'USD') {
-        product.price = product.priceUsd
-      } else {
-        product.price = await ctx.lib.call('currency', {from: product.priceUsd, toCode: getUserCurrency(ctx)})
-      }
+      await convertProductPrize(ctx, product)    
     }
 
     const options = {
@@ -50,15 +60,16 @@ module.exports = lib.serverless.router(async router => {
       user_currency: getUserCurrency(ctx),
       currencies: supportedCurrencies, 
       products: productList,
-      cart_size: cartSize,
+      cart_size: getCartSize(ctx),
       banner_color: 'white', // illustrates canary deployments
-      ads: cats
+      ads: cats,
+      href: ctx.request.href
     }
     ctx.type = 'text/html'
     ctx.body = homeHTML(options)
   })
 
-  router.post('/setCurrency(.*)', async (ctx, next) => {
+  router.post('/setCurrency', async (ctx, next) => {
     ctx.type = 'application/json'
     // chosenCurrency = ctx.request.body.currencyCode
     ctx.cookies.set('userCurrency', ctx.request.body.currencyCode, { overwrite:true })
@@ -68,7 +79,7 @@ module.exports = lib.serverless.router(async router => {
   // TODO make recommendations more meaningful? --> use categories?
   // Yes, IDs are required to be word shaped here
   router.get('/product/([A-Za-z0-9_]+)', async (ctx, next) => {
-    const productId = ctx.request.url.split("/").slice(-1)[0]
+    const productId = ctx.request.url.split("/").slice(-1)[0] || ctx.request.url.split("/").slice(-2,-1)[0]
      
     const requestId = lib.helper.generateRandomID()
     const product = await ctx.lib.call('getproduct', { id: productId })
@@ -80,11 +91,7 @@ module.exports = lib.serverless.router(async router => {
       return
     }
 
-    if (getUserCurrency(ctx) === 'USD') {
-      product.price = product.priceUsd
-    } else {
-      product.price = await ctx.lib.call('currency', {from: product.priceUsd, toCode: getUserCurrency(ctx)})
-    }
+    await convertProductPrize(ctx, product)
     const supportedCurrencies = (await ctx.lib.call('supportedcurrencies', {})).currencyCodes
     const recommendedIds = (await ctx.lib.call('listrecommendations', { userId: getUserName(ctx), productIds: [productId] })).productIds
     //const recommendedList = await recommendedIds.map(x => ctx.lib.call('getproduct', { id: x }))
@@ -98,10 +105,23 @@ module.exports = lib.serverless.router(async router => {
       user_currency: getUserCurrency(ctx),
       currencies: supportedCurrencies, 
       recommendations: recommendedIds,
-      cart_size: cartSize,
+      cart_size: getCartSize(ctx),
       ad: cat
     }
     ctx.type = 'text/html'
     ctx.body = productHTML(options)
   })
+
+  /*
+  router.get('/Cart', async (ctx, next) => {
+    let cartItems = (await ctx.lib.call('getCart', { userId: getUserName(ctx) })).items
+    cartItems = await cartItems.map(x => ctx.lib.call('getproduct', { id: x })
+    const shippingCost = ''
+    const totalCost = ''
+
+    ctx.type = 'text/html'
+    ctx.body = productHTML(options)
+  })
+  */
+
 })
