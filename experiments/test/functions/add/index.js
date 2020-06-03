e3496f1c7ebfc955056184927d7a38af7493fc2e
Christoph Witzko <github@christophwitzko.com>
2020-06-03 10:27:07 +0200

    feat(experiments/test): add workload config

--- experiments/test/functions/add/index.js
+++ experiments/test/functions/add/index.js
@@ -1,6 +1,6 @@
 const lib = require('@faastermetrics/lib')
 
-module.exports = lib.serverless.rpcHandler(event => {
-  lib.log({ event })
+module.exports = lib.serverless.rpcHandler((event, ctx) => {
+  ctx.log({ event })
   return { result: event.a + event.b }
 })
