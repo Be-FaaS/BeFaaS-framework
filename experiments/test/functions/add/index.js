20b268e4d540e772cec5889e33304bc92c7b87fd
Christoph Witzko <github@christophwitzko.com>
2020-06-12 16:28:05 +0200

    feat: update redis example

--- experiments/test/functions/add/index.js
+++ experiments/test/functions/add/index.js
@@ -1,12 +1,10 @@
 const lib = require('@faastermetrics/lib')
 
-const db = lib.db.connect()
-
 module.exports = lib.serverless.rpcHandler(async (event, ctx) => {
   const cacheKey = `${event.a}+${event.b}`
-  const res = await db.get(cacheKey)
-  if (res) return { result: parseInt(res, 10) }
+  const result = await ctx.db.get(cacheKey)
+  if (result) return { result }
   const sum = event.a + event.b
-  await db.set(cacheKey, `${sum}`)
+  await ctx.db.set(cacheKey, sum)
   return { result: sum }
 })
