6879cc1b378f0d18f0a09063a3adfe2c59bd41cb
Christoph Witzko <github@christophwitzko.com>
2020-06-14 18:16:31 +0200

    feat: update lib

--- experiments/test/functions/add/index.js
+++ experiments/test/functions/add/index.js
@@ -1,10 +1,13 @@
 const lib = require('@faastermetrics/lib')
 
-module.exports = lib.serverless.rpcHandler(async (event, ctx) => {
-  const cacheKey = `${event.a}+${event.b}`
-  const result = await ctx.db.get(cacheKey)
-  if (result) return { result }
-  const sum = event.a + event.b
-  await ctx.db.set(cacheKey, sum)
-  return { result: sum }
-})
+module.exports = lib.serverless.rpcHandler(
+  { db: 'redis' },
+  async (event, ctx) => {
+    const cacheKey = `${event.a}+${event.b}`
+    const result = await ctx.db.get(cacheKey)
+    if (result) return { result }
+    const sum = event.a + event.b
+    await ctx.db.set(cacheKey, sum)
+    return { result: sum }
+  }
+)
