7a850b296e8c2eab752a5fb9e06dc6e9c33565bc
Max Zhao <alcasa.mz@gmail.com>
2020-06-27 22:30:14 +0200

    feat: Add weathersensorfilter function.

---
+++ experiments/iot/functions/weathersensorfilter/index.js
@@ -0,0 +1,79 @@
+const lib = require('@faastermetrics/lib')
+
+/*
+ * Check that temperature is both valid and a number. Value is in celsius.
+ */
+function checkTemperature (temperature) {
+  if (temperature && !isNaN(temperature)) {
+    return temperature >= -273.15 && temperature <= 100.0
+  }
+  return false
+}
+
+/*
+ * Check that humidity is both valid and a number. Value is in %.
+ */
+function checkHumidity (humidity) {
+  if (humidity && !isNaN(humidity)) {
+    return humidity >= 0.0 && humidity <= 100.0
+  }
+  return false
+}
+
+/*
+ * Check that wind is both valid and a number. Value is in m/s.
+ */
+function checkWind (wind) {
+  if (wind && !isNaN(wind)) {
+    return wind >= 0.0 && wind <= 150.0
+  }
+  return false
+}
+
+/*
+ * Check that rain is a true boolean.
+ */
+function checkRain (rain) {
+  return typeof rain === 'boolean'
+}
+
+/*
+ * Filters incoming weather sensor data, such as removing erroneous data or NaN
+ * values.
+ *
+ * Ex Payload Body: {
+ *    "temperature": 10.0,
+ *    "humidity": 50.0,
+ *    "wind": 20.0,
+ *    "rain": true,
+ * }
+ *
+ * Response: { }
+ */
+module.exports = lib.serverless.rpcHandler(async (event, ctx) => {
+  const { temperature, humidity, wind, rain } = event
+
+  if (!checkTemperature(temperature)) {
+    return { error: 'Invalid temperature.' }
+  }
+
+  if (!checkHumidity(humidity)) {
+    return { error: 'Invalid humidity.' }
+  }
+
+  if (!checkWind(wind)) {
+    return { error: 'Invalid wind.' }
+  }
+
+  if (!checkRain(rain)) {
+    return { error: 'Invalid rain.' }
+  }
+
+  ctx.call('roadcondition', {
+    temperature: temperature,
+    humidity: humidity,
+    wind: wind,
+    rain: rain
+  })
+  return {}
+})
