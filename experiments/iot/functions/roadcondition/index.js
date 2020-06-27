90858e022ba1ce0ed403f262b945f3e0176a9007
Max Zhao <alcasa.mz@gmail.com>
2020-06-27 23:15:00 +0200

    feat: Add roadcondition function

---
+++ experiments/iot/functions/roadcondition/index.js
@@ -0,0 +1,58 @@
+const lib = require('@faastermetrics/lib')
+
+/*
+ * Calculate road condition. With 0 being perfect condition and 5 being worst
+ * condition.
+ */
+function calculateRoadCondition (temperature, humidity, wind, rain) {
+  let condition = 0
+  if (temperature < 4.0) {
+    condition += 1
+  }
+
+  if (humidity > 75.0) {
+    condition += 1
+  }
+
+  // beaufort 7
+  if (wind > 15.0) {
+    condition += 1
+  }
+
+  // beaufort 10
+  if (wind > 25.0) {
+    condition += 1
+  }
+
+  if (rain) {
+    condition += 1
+  }
+
+  return condition
+}
+
+/*
+ * Filters incoming weather sensor data, such as removing erroneous data or NaN
+ * values.
+ *
+ * Ex Payload Body: {
+ *    "temperature": 10.0,
+ *    "humidity": 50.0,
+ *    "wind": 20.0,
+ *    "rain": true,
+ * }
+ *
+ * Response: { }
+ */
+module.exports = lib.serverless.rpcHandler(async (event, ctx) => {
+  const { temperature, humidity, wind, rain } = event
+
+  const condition = calculateRoadCondition(temperature, humidity, wind, rain)
+
+  console.log(condition)
+
+  ctx.call('setlightphasecalculation', {
+    condition: condition
+  })
+  return {}
+})
