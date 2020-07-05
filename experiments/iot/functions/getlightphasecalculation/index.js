c2e2f28bd98e0315c3ab49fb2e92eb4d51cb10e8
Carsten Schubert <carsten.gm.schubert@campus.tu-berlin.de>
2020-07-05 18:57:06 +0200

    feat: added getLightPhaseCalculation

---
+++ experiments/iot/functions/getlightphasecalculation/index.js
@@ -0,0 +1,23 @@
+const lib = require('@faastermetrics/lib')
+
+/**
+ *
+ * Gets current traffic lights from the light calculation database.
+ *
+ * Example Request (empty): { }
+ *
+ * Example Response (empty): {
+ *   "lights": ["green", "yellow", "red"],
+ *   "blink": true
+ * }
+ *
+ */
+
+module.exports = lib.serverless.rpcHandler(
+  { db: 'redis' }, async (event, ctx) => {
+    const lights = await ctx.db.get('lightcalculation:lights')
+    const blink = (await ctx.db.get('lightcalculation:blink') === 'true'
+
+    return { lights, blink }
+  }
+)
--- experiments/iot/functions/setlightphasecalculation/index.js
+++ experiments/iot/functions/setlightphasecalculation/index.js
@@ -7,17 +7,17 @@ const _ = require('lodash')
  * Adapts to results of movement plan, emergency detection and road condition.
  * Writes light into database.
  *
- * Example Request (add): {
+ * Example Request: {
  *   "plan": [{
- *     "plate": "XXX"
- *     "direction": 3
+ *     "plate": "XXX",
+ *     "direction": 3,
  *     "speed": 63.8
- *   }]
+ *   }],
  * 
  *   "emergency": {
- *     "active": true
+ *     "active": true,
  *     "type": "lunatic"
- *   }
+ *   },
  * 
  *   "condition": 5
  * }
@@ -26,39 +26,39 @@ const _ = require('lodash')
  *
  */
 
-function initialDBUpdate (ctx, condition, plan, emergency) {
-  if (condition) ctx.db.set('lightcalculation:condition', _.toString(condition))
+async function initialDBUpdate (ctx, condition, plan, emergency) {
+  if (condition) await ctx.db.set('lightcalculation:condition', _.toString(condition))
 
   if (plan) {
-    ctx.db.set('lightcalculation:plates', _.map(plan, 'plate'))
-    ctx.db.set('lightcalculation:directions', _.map(_.map(plan, 'direction'), _.toString))
-    ctx.db.set('lightcalculation:speeds', _.map(_.map(plan, 'speed'), _.toString))
+    await ctx.db.set('lightcalculation:plates', _.map(plan, 'plate'))
+    await ctx.db.set('lightcalculation:directions', _.map(_.map(plan, 'direction'), _.toString))
+    await ctx.db.set('lightcalculation:speeds', _.map(_.map(plan, 'speed'), _.toString))
   }
 
   // Active emergencies are handled immediately
   if (emergency) {
     if (emergency.active) {
-      ctx.db.set('lightcalculation:light', ['yellow'])
-      ctx.db.set('lightcalculation:blink', 'true')
-      ctx.db.set('lightcalculation:emergency', 'true')
-      ctx.db.set('lightcalculation:emergencytype', emergency.type)
+      await ctx.db.set('lightcalculation:lights', ['yellow'])
+      await ctx.db.set('lightcalculation:blink', 'true')
+      await ctx.db.set('lightcalculation:emergency', 'true')
+      await ctx.db.set('lightcalculation:emergencytype', emergency.type)
     }
     else {
-      ctx.db.set('lightcalculation:emergency', 'false')
-      ctx.db.set('lightcalculation:emergencytype', '')
+      await ctx.db.set('lightcalculation:emergency', 'false')
+      await ctx.db.set('lightcalculation:emergencytype', '')
     }
   }
 }
 
-function checkAndLock (ctx) {
-  if ((ctx.db.get('lightcalculation:lock')) === 'true') return true
-  ctx.db.set('lightcalculation:lock', 'true')
+async function checkAndLock (ctx) {
+  if ((await ctx.db.get('lightcalculation:lock')) === 'true') return true
+  await ctx.db.set('lightcalculation:lock', 'true')
   return false
 }
 
-function checkAndUnlock (ctx) {
-  if ((ctx.db.get('lightcalculation:lock')) === 'false') return true
-  ctx.db.set('lightcalculation:lock', 'false')
+async function checkAndUnlock (ctx) {
+  if ((await ctx.db.get('lightcalculation:lock')) === 'false') return true
+  await ctx.db.set('lightcalculation:lock', 'false')
   return false
 }
 
@@ -71,54 +71,54 @@ async function waitAppropriately (ctx) {
   return new Promise(resolve => setTimeout(resolve, (condition + 2.5) * 1000));
 }
 
-function changeLight (ctx) {
+async function changeLight (ctx) {
   // Emergencies rule
-  const emergency = (ctx.db.get('lightcalculation:emergency') === 'true')
+  const emergency = (await ctx.db.get('lightcalculation:emergency') === 'true')
   if (emergency) {
-    ctx.db.set('lightcalculation:light', ['yellow'])
-    ctx.db.set('lightcalculation:blink', 'true')
+    await ctx.db.set('lightcalculation:lights', ['yellow'])
+    await ctx.db.set('lightcalculation:blink', 'true')
     return
   }
 
   // If no movement plan or cars just say lights are red blink (so pedestrians are happy)
-  const plates = ctx.db.get('lightcalculation:plates')
+  const plates = await ctx.db.get('lightcalculation:plates')
   // const directions = ctx.db.get('lightcalculation:directions')
   // We probably really should have 4 different traffic lights
-  const speeds = ctx.db.get('lightcalculation:speeds')
+  const speeds = await ctx.db.get('lightcalculation:speeds')
   if (_.isEmpty(plates) || ! _.find(_.map(speeds, _.toFinite), x => x > 50)) {
-    ctx.db.set('lightcalculation:light', ['yellow'])
-    ctx.db.set('lightcalculation:blink', 'false')
-    wait(1)
-    ctx.db.set('lightcalculation:light', ['red'])
-    ctx.db.set('lightcalculation:blink', 'false')
+    await ctx.db.set('lightcalculation:lights', ['yellow'])
+    await ctx.db.set('lightcalculation:blink', 'false')
+    await wait(1)
+    await ctx.db.set('lightcalculation:lights', ['red'])
+    await ctx.db.set('lightcalculation:blink', 'false')
   }
   else {
-    ctx.db.set('lightcalculation:light', ['yellow', 'red'])
-    ctx.db.set('lightcalculation:blink', 'false')
-    wait(1)
-    ctx.db.set('lightcalculation:light', ['green'])
-    ctx.db.set('lightcalculation:blink', 'false')
+    await ctx.db.set('lightcalculation:lights', ['yellow', 'red'])
+    await ctx.db.set('lightcalculation:blink', 'false')
+    await wait(1)
+    await ctx.db.set('lightcalculation:lights', ['green'])
+    await ctx.db.set('lightcalculation:blink', 'false')
   }
   
-  const newEmergency = (ctx.db.get('lightcalculation:emergency') === 'true')
+  const newEmergency = (await ctx.db.get('lightcalculation:emergency') === 'true')
   if (newEmergency) {
-    ctx.db.set('lightcalculation:light', ['yellow'])
-    ctx.db.set('lightcalculation:blink', 'true')
+    await ctx.db.set('lightcalculation:lights', ['yellow'])
+    await ctx.db.set('lightcalculation:blink', 'true')
   }
 }
 
 module.exports = lib.serverless.rpcHandler(
-  { db: 'redis' }, (event, ctx) => {
+  { db: 'redis' }, async (event, ctx) => {
     const { plan, emergency, condition } = event
     if (!plan && !emergency && !condition) return { error: 'Wrong payload.' }
 
-    initialDBUpdate(ctx, condition, plan, emergency)
+    await initialDBUpdate(ctx, condition, plan, emergency)
 
-    if (checkAndLock(ctx)) return { }
-    waitAppropriately(ctx)
-    if (checkAndUnlock(ctx)) return { }
+    if (await checkAndLock(ctx)) return { }
+    await waitAppropriately(ctx)
+    if (await checkAndUnlock(ctx)) return { }
 
-    changeLight(ctx)
+    await changeLight(ctx)
     return { }
   }
 )
