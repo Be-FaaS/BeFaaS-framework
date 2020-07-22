0e6169b840691266942e64b8a0b7ef66e63b1305
Christoph Witzko <github@christophwitzko.com>
2020-07-22 11:59:35 +0200

    fix: google log script

--- scripts/logs/google.sh
+++ scripts/logs/google.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 echo "Getting Google Cloud logs" | chalk magenta
-gcloud logging read 'resource.type="cloud_function"' --format json | jq -c '.[]' > $logdir/google.log
+gcloud logging read --project $GOOGLE_PROJECT 'resource.type="cloud_function"' --format json | jq -c '.[]' > $logdir/google.log
