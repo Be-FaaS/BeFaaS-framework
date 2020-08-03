baed44419d0f7e4554b65431d5cc0edd24b5c1de
Christoph Witzko <github@christophwitzko.com>
2020-08-03 10:20:45 +0200

    fix: notice script

--- scripts/notice.sh
+++ scripts/notice.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-if ! [ -f ".suppress_notice" ]; then
-    echo "
-If you use this software (or parts of it) in a published work, please include a citation similar to the following BibTex notice. 
+set -euo pipefail
+
+[ -f ".suppress_notice" ] && exit 0
+
+echo "
+If you use this software (or parts of it) in a published work, please include a citation similar to the following BibTex notice.
 We will update this entry once we have finished a complete software documentation for our work."
-    echo -e "
-\e[90m@Manual{faasterMetrics2020,
+
+chalk gray "
+@Manual{faasterMetrics2020,
   title = {FaaSter Metrics: An Application-oriented Function as a Service Benchmarking Framework},
   author = {Burchard, Luk and Schubert, Carsten and Witzko, Christoph and Zhao, Max and Dietrich, Emily},
   organization = {FaaSter Metrics project group},
   address = {Berlin, Germany},
   year = {2020},
   url = {https://github.com/FaaSterMetrics},
-}\e[39m
+}
+
 "
-    echo -e "To locally supress this notice please run  
-$ \e[32mtouch \"$PWD/.suppress_notice\"\e[39m
+
+chalk -t "
+To locally supress this notice please run:
+$ {green touch \"$PWD/.suppress_notice\"}
 "
-fi
