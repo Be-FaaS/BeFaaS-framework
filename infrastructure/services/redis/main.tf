109ee2f564b933bf864cea9253054b3c2095daa1
Martin Grambow <grambow@tu-berlin.de>
2020-11-17 23:25:48 +0100

    fix redis version to 6.0.7

--- infrastructure/services/redis/main.tf
+++ infrastructure/services/redis/main.tf
@@ -25,7 +25,7 @@ locals {
 
 data "aws_ami" "bitnami_redis" {
   most_recent = true
-  name_regex  = "^bitnami-redis-6.0.8-\\d-linux-debian-10-x86_64-hvm-ebs-nami$"
+  name_regex  = "^bitnami-redis-6.0.7-\\d-linux-debian-10-x86_64-hvm-ebs-nami$"
   owners      = ["979382823631"]
 }
 
