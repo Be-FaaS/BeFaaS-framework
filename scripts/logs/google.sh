5a5d1552ac3fe0603317afd920843a5ffb2db445
Martin Grambow <grambow@tu-berlin.de>
2020-11-19 12:45:45 +0100

    filter Google logs on deployment id

--- scripts/logs/google.sh
+++ scripts/logs/google.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 echo "Getting Google Cloud logs" | chalk magenta
-gcloud logging read --project $GOOGLE_PROJECT 'resource.type="cloud_function"' --format json | jq -c '.[]' > $logdir/google.log
+gcloud logging read --project $GOOGLE_PROJECT 'resource.type="cloud_function" AND textPayload:$BEFAAS_DEPLOYMENT_ID' --format json | jq -c '.[]' > $logdir/google.log
