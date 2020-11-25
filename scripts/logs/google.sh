a2bf4b24458ba088001cdf5cedc6160aa7c7702f
Martin Grambow <grambow@tu-berlin.de>
2020-11-25 18:21:16 +0100

    split "get google logs" into two commands capable to handle big data

--- scripts/logs/google.sh
+++ scripts/logs/google.sh
@@ -2,5 +2,10 @@
 
 set -euo pipefail
 
-echo "Getting Google Cloud logs" | chalk magenta
-gcloud logging read --project $GOOGLE_PROJECT 'resource.type="cloud_function" AND textPayload:'$BEFAAS_DEPLOYMENT_ID --format json | jq -c '.[]' > $logdir/google.log
+echo "Download Google Cloud logs, part 1/2 (very slow)" | chalk magenta
+
+gcloud logging read --project $GOOGLE_PROJECT 'resource.type="cloud_function" AND textPayload:'$BEFAAS_DEPLOYMENT_ID --format json > $logdir/googleraw.json
+
+echo "Getting Google Cloud logs, part 2 (faster)" | chalk magenta
+
+cat $logdir/googleraw.json | jq -c -s -R '.[]' > $logdir/google.log
