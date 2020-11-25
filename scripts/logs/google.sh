1b06be8e5bf374a0f6de8452bb0cfa970e9e122b
Martin Grambow <grambow@tu-berlin.de>
2020-11-25 23:25:24 +0100

    no need for jq here, it only overflows the RAM with log entries

--- scripts/logs/google.sh
+++ scripts/logs/google.sh
@@ -2,10 +2,6 @@
 
 set -euo pipefail
 
-echo "Download Google Cloud logs, part 1/2 (very slow)" | chalk magenta
+echo "Download Google Cloud logs (very slow)" | chalk magenta
 
-gcloud logging read --project $GOOGLE_PROJECT 'resource.type="cloud_function" AND textPayload:'$BEFAAS_DEPLOYMENT_ID --format json > $logdir/googleraw.json
-
-echo "Getting Google Cloud logs, part 2 (faster)" | chalk magenta
-
-cat $logdir/googleraw.json | jq -c -s -R '.[]' > $logdir/google.log
+gcloud logging read --project $GOOGLE_PROJECT 'resource.type="cloud_function" AND textPayload:'$BEFAAS_DEPLOYMENT_ID --format json > $logdir/google.log
