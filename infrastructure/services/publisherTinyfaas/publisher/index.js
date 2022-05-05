1a29a1f01c813ee04fd7eeeb22e818a210675a91
Martin Grambow <grambow@tu-berlin.de>
2022-05-05 18:11:39 +0200

    publisher calls functions running on the same machine but does not wait for an answer

--- infrastructure/services/publisherTinyfaas/publisher/index.js
+++ infrastructure/services/publisherTinyfaas/publisher/index.js
@@ -1,10 +1,39 @@
 const lib = require('@befaas/lib')
+const fetch = require('node-fetch')
 
 module.exports = lib.serverless.rpcHandler(async (request, ctx) => {
   //null -> include all attributes; 2 -> format using 2 spaces
-  console.log("Request: \n" + JSON.stringify(request));
-  console.log("Context: \n" + JSON.stringify(ctx));
-  console.log("All Vars:" +  JSON.stringify(process.env))
+  console.log("Request: " + JSON.stringify(request));
+  console.log("Context: " + JSON.stringify(ctx));
+  console.log("All Vars: " +  JSON.stringify(process.env))
+  
+  
+  var txt = JSON.stringify(request.event);
+  if (txt.length == 0) {
+	  txt = "no message"
+  }
+  var contextId = ctx.contextId;
+  var xPair = ctx.xPair;
+  var functionName = request.fun;
+  
+  console.log("txt: " + txt);
+  console.log("fnName: " + functionName);
+  
+  try {  
+    fetch(`http://ec2-18-192-21-232.eu-central-1.compute.amazonaws.com:80/${functionName}/call`, {
+        method: 'post',
+        body: JSON.stringify(request.event || {}),
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Context': contextId,
+          'X-Pair': xPair
+        }
+      })
+  } catch (e) {
+	  console.log(e);
+  }
+  
+  console.log("Called function " + functionName);
   
   //Respond ok  
   return {
