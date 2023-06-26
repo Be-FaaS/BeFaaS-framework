13ba377cf40d987e8d5e1905ee5dba670b2a66d0
Martin Grambow <grambow@tu-berlin.de>
2023-06-26 14:36:22 +0200

    add log statements to debug Azure publisher

--- infrastructure/services/publisherAzure/publisher/index.js
+++ infrastructure/services/publisherAzure/publisher/index.js
@@ -2,8 +2,8 @@ const lib = require('@befaas/lib')
 const { EventGridPublisherClient, AzureKeyCredential } = require("@azure/eventgrid");
 
 module.exports = lib.serverless.rpcHandler(async (request, ctx) => {
-  // console.log("Request: " + JSON.stringify(request));
-  // console.log("Context: " + JSON.stringify(ctx));
+  console.log("Request: " + JSON.stringify(request));
+  console.log("Context: " + JSON.stringify(ctx));
 
   var txt = JSON.stringify(request.event);
   if (txt.length == 0) {
@@ -20,16 +20,16 @@ module.exports = lib.serverless.rpcHandler(async (request, ctx) => {
   var idx = -1;
 
   for (let i = 0; i < endpoints.length; i++) {
-    //console.log(i.toString() + ": " + endpoints[i]  + ": " + keys[i])
+    console.log(i.toString() + ": " + endpoints[i]  + ": " + keys[i])
 	  if (endpoints[i].startsWith("https://" + functionName + ".")) {
 		  idx = i;
 	  }
   }
 
-  //console.log("txt: " + txt);
-  //console.log("fnName: " + functionName);
-  //console.log("endpoint: " + endpoints[idx]);
-  //console.log("key: " + keys[idx]);
+  console.log("txt: " + txt);
+  console.log("fnName: " + functionName);
+  console.log("endpoint: " + endpoints[idx]);
+  console.log("key: " + keys[idx]);
 
   const client = new EventGridPublisherClient(
     endpoints[idx],
@@ -49,6 +49,8 @@ module.exports = lib.serverless.rpcHandler(async (request, ctx) => {
     }
   }
   ]);
+
+  console.log("published.")
 
   return {
     statusCode: 200,
